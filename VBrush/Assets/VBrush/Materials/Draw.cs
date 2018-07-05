d7903c0c527a313eb808d3590b5417eae7aea995
DanWaffle <danut.nitu@outlook.com>
2018-07-05 18:14:09 +0100

    First version, will experiment with multi targets

---
+++ VBrush/Assets/VBrush/Materials/Draw.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Draw : MonoBehaviour, IVirtualButtonEventHandler{
+    private bool isPressed = false;
+    public GameObject VBtn;
+    public GameObject Paint;
+    public GameObject Brush;
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        isPressed = true;
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        isPressed = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        VBtn = GameObject.Find("VButton");
+        Brush = GameObject.Find("brush");
+        VBtn.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPressed == true)
+        {
+            Instantiate(Paint, Brush.transform.position, Brush.transform.rotation);
+        }
+	}
+}
---
+++ VBrush/Assets/VBrush/Materials/Paint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paint : MonoBehaviour {
+    public GameObject target;
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("ARCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target.transform);
+		
+	}
+}
